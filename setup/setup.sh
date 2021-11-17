2cb449d79360f4a72e3bd8ba1e90775c3d9f8bdb
fforoozan <faraz.foroozan@vodafoneziggo.com>
2021-11-17 20:37:36 +0100

    Add Calico CNI

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,6 +2,10 @@
 # Script bootstraps k8s cluster with Nginx ingress controller and Metallb
 kind create cluster --config=setup/config.yaml
 
+# https://docs.projectcalico.org/getting-started/kubernetes/self-managed-onprem/onpremises#install-calico-on-nodes 
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+
 # https://kind.sigs.k8s.io/docs/user/loadbalancer/
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" 
